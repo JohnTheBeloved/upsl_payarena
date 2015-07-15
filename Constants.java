75870fd8347a60761e83c71278a01ff328c234a9
John Alade <john.alade@interswitchng.com>
2015-07-15 02:00:42 -0700

    Changed Config

--- Constants.java
+++ Constants.java
@@ -7,14 +7,14 @@ public class Constants{
 	public static final String APPROVE_URL = "https://www.mysite.com/approveuri";
 	public static final String CANCEL_URL = "https://www.mysite.com/canceluri";
 	public static final String DECLINE_URL = "https://www.mysite.com/declineuri";
-	public static final String MERCHANT_ID = "127.0.0.1";
+	public static final String MERCHANT_ID = "UNIGATEWAYTEST1";
 
 
  
 	//Non Mandatory Feilds
 
 
-	public static final String URL_TO_CONNECT = "https://196.46.20.36:5443/Exec";
+	public static final String URL_TO_CONNECT = "https://196.46.20.36:5444/Exec";
 	public static final String PINPAD_URL = "https://www.mysite.com/runtran.jsp";
 
 	public static final String ENCRYPTION_KEY = "0123456789abcdef";
